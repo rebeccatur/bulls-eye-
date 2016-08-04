274ef036e69227b4eaf533a9218493d7a970f333
Rebecca Turyan <rturyan7365@bths.edu>
2016-08-04 09:44:45 -0400

    bulls eye project

---
+++ bulls-eye/sketch.js
@@ -0,0 +1,22 @@
+
+
+
+function setup() {
+  createCanvas(640, 480);
+  background(0)
+  fill(255,0,0);
+  ellipse(300,300,75,75);
+  fill(255,255,255);
+  ellipse(300, 300, 50, 50);
+fill(255, 0, 0);
+ellipse(300, 300, 25, 25);
+}
+
+function draw() {
+ 
+
+
+
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+  rect(10,10,10,15);
+}
+
+function draw() {
+  
+}
